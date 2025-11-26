2796296f5b3e294b986ead711792fadadb9fc1be
Markus Neteler <neteler@gmail.com>
2025-11-26 17:11:31 +0100

    black

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    Dummy conftest.py for actinia_module_plugin.
+Dummy conftest.py for actinia_module_plugin.
 
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
-    https://pytest.org/latest/plugins.html
+If you don't know what this is for, just leave it empty.
+Read more about conftest.py under:
+https://pytest.org/latest/plugins.html
 """
 from __future__ import print_function, absolute_import, division
 
