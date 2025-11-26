ad3e5439f3a190e7c543da6bf714a4015c22d5bf
Markus Neteler <neteler@gmail.com>
2025-11-26 17:08:36 +0100

    SPDX: license declaration cleanup
    This PR fixes license declarations and adds missing declarations in file headers. It also adds the official license text file.
    
    Goal: acceptable result with `reuse lint`

--- src/actinia_module_plugin/api/processing.py
+++ src/actinia_module_plugin/api/processing.py
@@ -1,48 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Copyright (c) 2018-2021 mundialis GmbH & Co. KG
+SPDX-FileCopyrightText: (c) 2016-2025, Sören Gebbert and mundialis GmbH & Co. KG
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+SPDX-License-Identifier: GPL-3.0-or-later
 
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-Code based on actinia_core: github.com/mundialis/actinia_core
-
-# actinia-core - an open source REST API for scalable, distributed, high
-# performance processing of geographical data that uses GRASS GIS for
-# computational tasks. For details, see https://actinia.mundialis.de/
-#
-# Copyright (c) 2019-2025 Sören Gebbert and mundialis GmbH & Co. KG
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-
+Code based on actinia_core: https://github.com/actinia-org/actinia-core
+actinia-core - an open source REST API for scalable, distributed, high
+performance processing of geographical data that uses GRASS GIS for
+computational tasks. For details, see https://actinia.mundialis.de/
 
 Extension of actinia_cores ephemeral_processing_with_export
 AsyncEphemeralExportResource to include process chain templates
 """
 
-__license__ = "Apache-2.0"
+__license__ = "GPLv3"
 __author__ = "Anika Bettge, Sören Gebbert"
 __copyright__ = "Copyright 2016-2025, Sören Gebbert, mundialis GmbH & Co. KG"
 __maintainer__ = "mundialis"
